ea76ed3ce860abc74eb1f22fb8551c5a3f16c6d2
kyp092 <126074958+kyp092@users.noreply.github.com>
2023-03-04 22:12:06 +0530

    Update urls.py

--- hub/urls.py
+++ hub/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:primary_key>/', views.event_detail, name='event_detail'),
-
+   path('', views.DIY_index, name='DIY_index'),
+    path('DIY_index', views.DIY_index, name='DIY_index'),
+    path('DIY_signup',views.DIY_signup, name ='DIY_signup'),
+    path('DIY_user_page', views.DIY_user_page, name = 'DIY_user_page'),
+    path('DIY_create', views.DIY_create, name = 'DIY_create')
 ]
